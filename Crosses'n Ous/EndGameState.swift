27e2e5f7587a73725c452ce4e972cc470178738a
Astemir Eleev <astemireleev@gmail.com>
2017-07-01 17:28:59 +0300

    Implemented EndGameState class

---
+++ Crosses'n Ous/EndGameState.swift
@@ -0,0 +1,33 @@
+//
+//  EndGameState.swift
+//  Crosses'n Ous
+//
+//  Created by Astemir Eleev on 01/07/2017.
+//  Copyright © 2017 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+import GameplayKit
+
+class EndGameState: GKState{
+    var scene: GameScene?
+    
+    init(scene: GameScene){
+        self.scene = scene
+        super.init()
+    }
+    
+    override func isValidNextState(_ stateClass: AnyClass) -> Bool {
+        return stateClass == StartGameState.self
+    }
+    
+    override func didEnter(from previousState: GKState?) {
+        updateGameState()
+    }
+    
+    func updateGameState(){
+        let resetNode = self.scene?.childNode(withName: "Reset")
+        resetNode?.isHidden = false
+    }
+}
+
