c92a637a6ba97ef3eab760b84e7de0d7faa90379
Astemir Eleev <astemireleev@gmail.com>
2017-07-01 17:30:07 +0300

    Changed current device orientation to portrait

--- Crosses'n Ous/GameViewController.swift
+++ Crosses'n Ous/GameViewController.swift
@@ -38,7 +38,7 @@ class GameViewController: UIViewController {
 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
+            return .portrait
         } else {
             return .all
         }
